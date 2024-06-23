773598a3a383b275e3fd3c8766cff943c16d3a30
Hassaankh10 <m.hassaan10.mh10@gmail.com>
2024-06-23 19:31:54 +0500

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Video_Player
+//
+//  Created by Muhammad hassaan Khan on 21/06/2024.
+//
+
+import UIKit
+import AVKit
+import AVFoundation
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        playvideo();
+    }
+    private func playvideo(){
+        guard let
+                path=Bundle.main.path(forResource: "3195394-uhd_3840_2160_25fps", ofType: "mp4") else {
+            debugPrint("Unable to find specified video")
+            return
+        }
+        let player = AVPlayer(url: URL(fileURLWithPath: path))
+        let playerController = AVPlayerViewController()
+        playerController.player=player
+        present(playerController, animated: true){
+            player.play()
+        }
+    }
+}
